4391f04eab972fc6387050b502f99c2d72eced7d
15bonte <thomas.bonte95@gmail.com>
2023-10-04 11:53:30 +0200

    Add corresponding widgets

--- src/cut_detector/_widget.py
+++ src/cut_detector/_widget.py
@@ -9,50 +9,61 @@ Replace code below according to your needs.
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+
+from cut_detector.utils.tools import re_organize_channels
+from cut_detector.widget_functions.mitosis_track_generation import perform_mitosis_track_generation
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+@magic_factory(
+    call_button="Run Mitosis Track Generation",
+    layout="vertical",
+    xml_model_dir=dict(
+        widget_type="FileEdit",
+        label=".xml models directory: ",
+    ),
+    mitoses_save_dir=dict(
+        widget_type="FileEdit",
+        label="Mitoses saved path: ",
+    ),
+    tracks_save_dir=dict(
+        widget_type="FileEdit",
+        label="Tracks saved path: ",
+    ),
+)
+def mitosis_track_generation(
+    img_layer: "napari.layers.Image",
+    xml_model_dir: str,
+    mitoses_save_dir: str,
+    tracks_save_dir: str,
+):
+    raw_video = re_organize_channels(img_layer.data)  # TXYC
+    perform_mitosis_track_generation(
+        raw_video, img_layer.name, xml_model_dir, mitoses_save_dir, tracks_save_dir
+    )
 
 
 @magic_factory(
-    call_button="Run MT cut detection",
+    call_button="Run Mid-body Detection",
     layout="vertical",
     mitoses_path=dict(
         widget_type="FileEdit",
         label="Mitoses saved path: ",
     ),
 )
-def micro_tubules_cut_detection(
-    img_layer: "napari.layers.Image", mitoses_path: str
-):
-    raw_video = img_layer.data
-    # print first value
-    print(raw_video.shape)
-    print(raw_video[0, 0, 0, 0])
+def mid_body_detection(img_layer: "napari.layers.Image", mitoses_path: str):
+    raw_video = re_organize_channels(img_layer.data)  # TXYC
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+@magic_factory(
+    call_button="Run MT Cut Detection",
+    layout="vertical",
+    mitoses_path=dict(
+        widget_type="FileEdit",
+        label="Mitoses saved path: ",
+    ),
+)
+def micro_tubules_cut_detection(img_layer: "napari.layers.Image", mitoses_path: str):
+    raw_video = re_organize_channels(img_layer.data)  # TXYC
--- src/cut_detector/utils/tools.py
+++ src/cut_detector/utils/tools.py
@@ -1,5 +1,5 @@
 import sys
-import psutil
+import numpy as np
 
 
 def display_progress(
@@ -8,20 +8,39 @@ def display_progress(
     total,
     precision=1,
     additional_message="",
-    cpu_memory=False,
-):
+) -> None:
+    """
+    Nice display of progress.
+    """
     percentage = round(current / total * 100, precision)
     padded_percentage = str(percentage).ljust(precision + 3, "0")
     display_message = f"\r{message}: {padded_percentage}%"
     # Display additional message
     if additional_message:
         display_message += " | " + additional_message
-    # Display CPU memory usage
-    if cpu_memory:
-        cpu_available = round(
-            psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
-        )
-        cpu_message = f"CPU available: {cpu_available}%"
-        display_message += " | " + cpu_message
     sys.stdout.write(display_message)
     sys.stdout.flush()
+
+
+def re_organize_channels(image: np.ndarray) -> np.ndarray:
+    """
+    Expect a 4 dimensions image.
+    Re-organize channels to get TXYC order.
+    """
+    if image.ndim != 4:
+        raise ValueError("Expect a 4 dimensions image.")
+
+    # Get dimension index of smallest dimension, i.e. channels
+    channels_dimension_index = np.argmin(image.shape)
+    channels_dimension = image.shape[channels_dimension_index]
+    if channels_dimension != 3:
+        raise ValueError("Expect 3 channels: SiR-tubulin/MKLP1/Phase contrast, in that order.")
+    # Put channels at the back
+    image = np.moveaxis(image, channels_dimension_index, 3)
+
+    # Get dimension index of second smallest dimension, i.e. time
+    second_dimension_index = np.argsort(image.shape)[1]
+    # Put time at the front
+    image = np.moveaxis(image, second_dimension_index, 0)
+
+    return image
