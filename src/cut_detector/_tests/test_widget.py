c74f11c6cf8ed39360f2c2535a9bb922ae6e8454
15bonte <thomas.bonte95@gmail.com>
2023-10-16 18:51:18 +0200

    Add removed widget

---
+++ src/cut_detector/_tests/test_widget.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from cut_detector._widget import whole_process
+
+
+def test_example_magic_widget(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+    layer = viewer.add_image(np.random.random((100, 100)))
+
+    # this time, our widget will be a MagicFactory or FunctionGui instance
+    my_widget = whole_process()
+
+    # if we "call" this object, it'll execute our function
+    my_widget(viewer.layers[0])
+
+    # read captured output and check that it's as we expected
+    captured = capsys.readouterr()
+    assert captured.out == f"you have selected {layer}\n"
