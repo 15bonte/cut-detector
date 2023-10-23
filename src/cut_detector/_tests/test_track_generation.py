ea4276327c52cfb0989f206c39de77bbb34d00eb
15bonte <thomas.bonte95@gmail.com>
2023-10-23 17:58:17 +0200

    Create test for track generation

---
+++ src/cut_detector/_tests/test_track_generation.py
@@ -0,0 +1,29 @@
+import os
+from skimage import io
+
+from cut_detector._widget import mitosis_track_generation
+from cut_detector.data.tools import get_data_path
+
+
+def test_open_track_generation_widget():
+    # Just try to open the widget
+    mitosis_track_generation()
+
+
+def test_track_generation_widget(make_napari_viewer_proxy):
+    viewer = make_napari_viewer_proxy()
+
+    # Add video
+    video = io.imread(os.path.join(get_data_path("videos"), "example_video.tif"))
+    viewer.add_image(video)
+
+    # Open the widget
+    widget = mitosis_track_generation()
+
+    # Run process
+    widget(
+        viewer.layers[0],
+        get_data_path("models"),
+        get_data_path("mitoses"),
+        get_data_path("tracks"),
+    )
---
+++ src/cut_detector/_tests/test_whole_process.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from cut_detector._widget import whole_process
+
+
+def test_open_whole_process_widget():
+    # Just try to open the widget
+    whole_process()
