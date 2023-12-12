95d920c1eb536b104179435dcbec843f1372939e
15bonte <thomas.bonte95@gmail.com>
2023-12-12 14:19:21 +0100

    Prepare mitosis mask layer display

--- playground/napari_layer.py
+++ playground/napari_layer.py
@@ -1,10 +1,22 @@
+import os
+from skimage import io
 import numpy as np
 import napari
+import pickle
+
+from cut_detector.data.tools import get_data_path
+from cut_detector.utils.mitosis_track import MitosisTrack
 
 
 def main():
     # Create a Napari viewer
     viewer = napari.Viewer()
+
+    # Add video
+    video = io.imread(
+        os.path.join(get_data_path("videos"), "example_video.tif")
+    )
+    viewer.add_image(video, name="example_video")
 
     # Create a blue rectangle
     rectangle_width = 50
@@ -28,6 +40,20 @@ def main():
         edge_color="transparent",
         face_color=rectangle_color,
     )
+
+    # Load mitosis tracks
+    mitosis_tracks: list[MitosisTrack] = []
+    exported_mitoses_dir = get_data_path("mitoses")
+    for state_path in os.listdir(exported_mitoses_dir):
+        with open(os.path.join(exported_mitoses_dir, state_path), "rb") as f:
+            mitosis_track = pickle.load(f)
+        mitosis_tracks.append(mitosis_track)
+
+    # Iterate over mitosis_tracks
+    for mitosis_track in mitosis_tracks:
+        _, mask_movie = mitosis_track.generate_video_movie(video)
+        # Add mask_movie to viewer
+        # TODO
 
     # Display the Napari viewer
     napari.run()
