2db36552e8cd74e32b5e3a20db4df97197274fc3
15bonte <thomas.bonte95@gmail.com>
2024-03-07 11:52:08 +0100

    Modify napari_layer with working display

--- playground/napari_layer.py
+++ playground/napari_layer.py
@@ -22,28 +22,16 @@ def main(
     viewer.add_image(video[..., 1].squeeze(), name="mid-body")
     viewer.add_image(video[..., 2].squeeze(), name="phase contrast")
 
-    # Create a blue rectangle
-    rectangle_width = 50
-    rectangle_height = 30
-    rectangle_color = [0, 0, 1]  # Blue color in RGB
-
-    # Define the rectangle vertices
-    vertices = np.array(
-        [
-            [0, 0],
-            [rectangle_width, 0],
-            [rectangle_width, rectangle_height],
-            [0, rectangle_height],
-        ]
-    )
-
-    # Create a rectangle layer
-    viewer.add_shapes(
-        data=vertices,
-        shape_type="rectangle",
-        edge_color="transparent",
-        face_color=rectangle_color,
-    )
+    rectangle_mask = np.zeros((50, 600, 600, 3), dtype=np.uint8)
+    for _ in range(10):
+        color = np.random.randint(0, 255, 3)
+        random_position = np.random.randint(0, 500, 2)
+        rectangle_mask[
+            :,
+            random_position[0] : random_position[0] + 100,
+            random_position[1] : random_position[1] + 100,
+        ] = color
+    viewer.add_image(rectangle_mask, name="mask", opacity=0.4)
 
     # Load mitosis tracks
     mitosis_tracks: list[MitosisTrack] = []
