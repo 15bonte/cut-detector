915395da05a585362e16ff96099092b54a7268d2
Clara Pruneau <clara.pruneau@etu.minesparis.psl.eu>
2024-03-12 16:36:53 +0100

    premiere etape

--- playground/tracking.py
+++ playground/tracking.py
@@ -6,6 +6,8 @@ from cut_detector.data.tools import get_data_path
 from cut_detector.utils.trackmate_track import TrackMateTrack
 from cut_detector.utils.trackmate_spot import TrackMateSpot
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 def load_tracks_and_spots(
     trackmate_tracks_path: str, spots_path: str
@@ -48,7 +50,36 @@ def main(
     trackmate_tracks, trackmate_spots = load_tracks_and_spots(
         trackmate_tracks_path, spots_path
     )
+    
+    # Frame of interest
+    frame = 0
+
+    # Plot cellpose_results
+    print(cellpose_results[frame])
+    plt.figure()
+    plt.imshow(cellpose_results[frame])
+    plt.show()
+    plt.close()
+
+    # Plot trackmate_spots of frame number "frame"
+    y = []
+    x = []
+    
+    for s in trackmate_spots:
+        if s.frame == frame:
+            list = s.spot_points
+            for i in range(len(list)):
+                x.append(list[i][0])
+                y.append(600 - list[i][1])
+    plt.scatter(x,y)
+    plt.show()
+
+    # Finding barycenters of each cell
+    for i in range(1,2):
+        indices = np.where(cellpose_results[frame]==i)
+        #print(indices)
 
 
 if __name__ == "__main__":
     main()
+
