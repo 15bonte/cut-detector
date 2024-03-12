34647e5316c1f06ded62f5404fc770db5aecebd6
gwenaellehulard <gwenaelle.hulard@etu.minesparis.psl.eu>
2024-03-12 16:24:53 +0100

    débug previous code

--- playground/napari_layer.py
+++ playground/napari_layer.py
@@ -4,7 +4,6 @@ from skimage import io
 import numpy as np
 import napari
 import pickle
-
 from cut_detector.data.tools import get_data_path
 from cut_detector.utils.mitosis_track import MitosisTrack
 
@@ -21,29 +20,7 @@ def main(
     viewer.add_image(video[..., 0].squeeze(), name="micro-tubules")
     viewer.add_image(video[..., 1].squeeze(), name="mid-body")
     viewer.add_image(video[..., 2].squeeze(), name="phase contrast")
-
-    # Create a blue rectangle
-    rectangle_width = 50
-    rectangle_height = 30
-    rectangle_color = [0, 0, 1, 0]  # Blue color in RGB
-
-    # Define the rectangle vertices
-    vertices = np.array(
-        [
-            [0, 0],
-            [rectangle_width, 0],
-            [rectangle_width, rectangle_height],
-            [0, rectangle_height],
-        ]
-    )
-
-    # Create a rectangle layer # affichage rectangle bleu
-    viewer.add_shapes(
-        data=vertices,
-        shape_type="rectangle",
-        edge_color="transparent",
-        face_color=rectangle_color,
-    )
+    nbframes, height, width, _ = video.shape
 
     # Load mitosis tracks  # masques rajoutés qui suivent les cellules
     mitosis_tracks: list[MitosisTrack] = []
@@ -53,23 +30,24 @@ def main(
         mitosis_tracks.append(mitosis_track)
 
     # Colors list
-    colors = np.array([[np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255), 0.3] for i in range(100)])
+    colors = np.array([[np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255)] for i in range(len(mitosis_tracks))], dtype=np.uint8)
 
     # Iterate over mitosis_tracks
-    mask = np.zeros((50,600,600,4))
-    for mitosis_track in mitosis_tracks:
+    mask = np.zeros((nbframes,height,width,3), dtype=np.uint8)
+    print("generate mask")
+    for i,mitosis_track in enumerate(mitosis_tracks):
         _, mask_movie = mitosis_track.generate_video_movie(video)
-        empty_indexes = np.where(mask_movie == 0)
+        
         cell_indexes = np.where(mask_movie == 1)
         
-        mask_movie = np.stack([mask_movie,mask_movie,mask_movie,mask_movie],axis=-1)
-        mask_movie[empty_indexes] = [0,0,0,0]
-        mask_movie[cell_indexes] = [255,255,255,0.3]
+        mask_movie = np.stack([mask_movie,mask_movie,mask_movie],axis=-1)
+        
+        mask_movie[cell_indexes] = colors[i]
         
         # Add mask_movie to viewer
         mask[mitosis_track.min_frame:mitosis_track.max_frame+1, mitosis_track.position.min_y:mitosis_track.position.max_y, mitosis_track.position.min_x:mitosis_track.position.max_x,:] = mask_movie
     
-    viewer.add_image(mask, name="masks", rgb=True)
+    viewer.add_image(mask, name="masks", rgb=True, opacity=0.4)
         
 
     # Display the Napari viewer
@@ -77,4 +55,7 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        image_path = r"C:\Users\gwenaelle\git\cut-detector",
+        mitoses_path = r"C:\Users\gwenaelle\git\cut-detector\mitoses"
+    )
