16100e32bd6cfccc9b1c80518f1d8e6e0da22af8
PaulGMines <pgagna@outlook.fr>
2024-03-15 18:25:53 +0100

    Adding LapOfGauss Visualization
    Adding LapOfGauss visualization using a modified versions of skimage's blob_log

--- src/cut_detector/mb_blob/layers/blob_detect.py
+++ src/cut_detector/mb_blob/layers/blob_detect.py
@@ -1,21 +1,22 @@
 """ The Gaussian Layers
 """
 
-# from skimage.morphology import extrema, opening
+import matplotlib.pyplot as plt
 from skimage.feature import blob_log
 from .layer import BlobLayer
-
-class LapOfGaussVisu:
-    pass
+from .lapgau_helpers import blob_log_with_plotting, BlobLogVisuSettings
 
 class LapOfGauss(BlobLayer):
     """Applies a Laplacian of Gaussian to detect blobs"""
-    def __init__(self, min_sig: int, max_sig: int, n_sig: int, threshold: float = 0.2, visu: LapOfGaussVisu = None) -> None:
+    def __init__(self, min_sig: int, max_sig: int, n_sig: int, threshold: float = 0.2, visu: BlobLogVisuSettings = None) -> None:
         self.min_sig = min_sig
         self.max_sig = max_sig
         self.n_sig = n_sig
         self.threshold = threshold
-        self.visu = visu
+        if visu is None:
+            self.visu = BlobLogVisuSettings()
+        else:
+            self.visu = visu
 
     def apply(self, env: dict):
         img = env["img"]
@@ -28,8 +29,17 @@ class LapOfGauss(BlobLayer):
                 threshold=self.threshold
             )
         else:
-            blobs = None
-            raise RuntimeError("Visualisation for blob_log not available for now")
+            blobs = blob_log_with_plotting(
+                img, 
+                min_sigma=self.min_sig,
+                max_sigma=self.max_sig,
+                num_sigma=self.n_sig,
+                threshold=self.threshold,
+                plot_settings=self.visu,
+            )
+            if self.visu.cube_plotting_threshold is not None or len(self.visu.sig_layers_idx) != 0:
+                plt.show()
+                
         env["blobs"] = blobs
 
 class DiffOfGauss(BlobLayer):
