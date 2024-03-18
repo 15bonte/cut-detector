5e2f287356fe1e493fdf091489a566ef6c6857ac
PaulGMines <pgagna@outlook.fr>
2024-03-18 10:35:28 +0100

    NLS - New PlotSaver
    Adding a way to save a plot.

--- src/cut_detector/mb_blob/layers/__init__.py
+++ src/cut_detector/mb_blob/layers/__init__.py
@@ -5,7 +5,7 @@ from .branch import OnFrame, Sequence
 from .environment import WriteImg, WriteTime
 from .normalization import MaxNormalizer, MinMaxNormalizer, HardBinaryNormalizer 
 from .normalization import AreaOpeningNormalizer, AreaClosingNormalizer
-from .plotting import PlotImage, PlotBlobs
+from .plotting import PlotImage, PlotBlobs, PlotSaver
 from .saving import SaveStdBlobMetrics
 
 from .blob_detect import BlobLogVisuSettings
--- src/cut_detector/mb_blob/layers/plotting.py
+++ src/cut_detector/mb_blob/layers/plotting.py
@@ -5,6 +5,21 @@ import sys
 import matplotlib.pyplot as plt
 from .layer import BlobLayer
 
+class PlotSaver(BlobLayer):
+    def __init__(self, filedir: str, filename: str, add_src_fp: bool = False):
+        self.filedir = filedir
+        self.filename = filename
+        self.add_src_fp = add_src_fp
+    
+    def apply(self, env: dict):
+        src_fp = env["source_fp"].split("/")[-1]
+        frame  = env["frame"]
+        if src_fp is None:
+            filepath = f"{self.filedir}/{self.filename}_f{frame}.png"
+        else:
+            filepath = f"{self.filedir}/{src_fp}_{self.filename}_f{frame}.png"
+        plt.savefig(filepath)
+
 class PlotImage(BlobLayer):
     """Plots a simple image, by default 'img' but that can be changed
     
@@ -27,14 +42,26 @@ class PlotBlobs(BlobLayer):
     Plotting is done in a blocking way
     """
 
-    def __init__(self, blobs_key: str = "blobs", srcimg_key: str = "src", sbs: bool = False):
+    def __init__(
+            self, 
+            blobs_key: str = "blobs", 
+            srcimg_key: str = "src", 
+            sbs: bool = False, 
+            title_prefix: str = "Blobs Found",
+            plt_saver: PlotSaver | None = None,
+            should_show: bool = True
+            ):
         self.blobs_key = blobs_key
         self.src_key = srcimg_key
         self.sbs = sbs
+        self.title_prefix = title_prefix
+        self.plt_saver = plt_saver
+        self.should_show = should_show
     
     def apply(self, env: dict):
         blobs = env.get(self.blobs_key, [])
         src_img = env.get(self.src_key)
+        frame = env["frame"]
         if src_img is None:
             # print(f"Could not find key {self.src_key} in env:\n", env, file=sys.stderr)
             raise RuntimeError(f"Could not find key {self.src_key} in environment")
@@ -48,4 +75,12 @@ class PlotBlobs(BlobLayer):
                 y, x, r = blob
                 circle = plt.Circle((x, y), r, linewidth=2, fill=False)
                 ax.add_patch(circle)
-        plt.show()
+
+        plt.title(f"{self.title_prefix} frame {frame}")
+        if not self.plt_saver is None:
+            self.plt_saver.apply(env)
+        
+        if self.should_show:
+            plt.show()
+        else:
+            plt.clf()
