0b760939a593b41933fedfc43569d8884a197883
PaulGMines <pgagna@outlook.fr>
2024-04-02 16:37:23 +0200

    Environment Wrapper
    This is a centralized version of the constants that were used in the playground projects

---
+++ bpkg/wrapper_env.py
@@ -0,0 +1,18 @@
+from importation import Source, SourceFiles
+from cut_detector.factories.mid_body_detection_factory import MidBodyDetectionFactory
+
+from pathlib import Path
+from os.path import join
+
+class WrapperEnv:
+    src_file: Source = SourceFiles.example
+    main_spot_detection_method = "lapgau"
+    out_dir: str = "src/cut_detector/data/mid_bodies_movies_test"
+    reference_detection_method: MidBodyDetectionFactory.SPOT_DETECTION_MODE = "lapgau"
+
+    @staticmethod
+    def gt_filepath_from_source(src: Source) -> str:
+        p = Path(src.path)
+        gt_out_dir = join(WrapperEnv.out_dir, "gt")
+        return join(gt_out_dir, f"{p.stem}__gt.json")
+
