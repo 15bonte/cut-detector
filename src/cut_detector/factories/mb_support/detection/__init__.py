3f5b683dbcbca8d08622e82ee6a05aacfc1705f1
PaulGMines <pgagna@outlook.fr>
2024-04-03 12:07:14 +0200

    Moving mb_support
    Moving mb_support next to mid_body_detection_factory

---
+++ src/cut_detector/factories/mb_support/detection/__init__.py
@@ -0,0 +1,2 @@
+from .detection_impl import current_log, current_dog, current_doh
+from .detection_func import detect_minmax_log, detect_minmax_dog, detect_minmax_doh
---
+++ src/cut_detector/factories/mb_support/tracking/__init__.py
@@ -0,0 +1,1 @@
+from .spatial_laptrack import SpatialLapTrack
