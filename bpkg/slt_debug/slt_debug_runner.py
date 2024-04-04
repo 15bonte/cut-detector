91533e172e3547136a4e0f7373e05badc84e64f8
PaulGMines <pgagna@outlook.fr>
2024-04-04 13:31:17 +0200

    Slt Debug port
    porting slt_debug to new bpkg

--- bpkg/slt_debug/slt_debug_runner.py
+++ bpkg/slt_debug/slt_debug_runner.py
@@ -1,4 +1,12 @@
 from .slt_debug import spatial_laptrack_debug
+from wrapper_env import WrapperEnv
 
 def spatial_laptrack_debug_runner():
-    pass
+    spatial_laptrack_debug(
+        source           = WrapperEnv.src_file,
+        slt              = WrapperEnv.main_slt_tracking_method,
+        detection_method = WrapperEnv.main_spot_detection_method,
+        log_fp           = WrapperEnv.log_filepath_for_purpose("slt_debug.txt"),
+        out_dir          = WrapperEnv.out_dir,
+        show_tracking    = False
+    )
