771bd72681bcb3b043ee2a0ed4ab1bc9db7bc058
15bonte <thomas.bonte95@gmail.com>
2024-04-09 09:45:20 +0200

    Correct default segmentation model

--- src/cut_detector/widget_functions/tracking.py
+++ src/cut_detector/widget_functions/tracking.py
@@ -11,14 +11,17 @@ def perform_tracking(
     video_path: str,
     fiji_path: str,
     save_folder: str,
-    model_path: Optional[str] = os.path.join(
-        get_model_path("segmentation"), "segmentation_model"
-    ),
+    model_path: Optional[str],
     fast_mode: Optional[bool] = False,
 ) -> None:
     # Create save directory if it doesn't exist
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
+
+    if model_path is None:
+        model_path = os.path.join(
+            get_model_path("segmentation"), "segmentation_model"
+        )
 
     segmentation_tracking_factory = SegmentationTrackingFactory(model_path)
     segmentation_tracking_factory.perform_trackmate_tracking(
