d5c43f7b0d966856ac8c373e3b8298cb06b599be
15bonte <thomas.bonte95@gmail.com>
2024-05-30 12:24:45 +0200

    Add function to compute polygon centroid

--- src/cut_detector/utils/segmentation_tracking/mask_utils.py
+++ src/cut_detector/utils/segmentation_tracking/mask_utils.py
@@ -630,3 +630,30 @@ def simplify(p, interval, epsilon):
     return MaskPolygon(
         list(x_simplified), list(y_simplified), len(x_simplified)
     )
+
+
+def signed_area(x, y):
+    """Compute the signed area of a polygon."""
+    n = len(x)
+    a = 0.0
+    for i in range(n - 1):
+        a += x[i] * y[i + 1] - x[i + 1] * y[i]
+    return (a + x[n - 1] * y[0] - x[0] * y[n - 1]) / 2.0
+
+
+def centroid(x, y):
+    """Calculate the centroid of a polygon."""
+    area = signed_area(x, y)
+    ax = 0.0
+    ay = 0.0
+    n = len(x)
+    for i in range(n - 1):
+        w = x[i] * y[i + 1] - x[i + 1] * y[i]
+        ax += (x[i] + x[i + 1]) * w
+        ay += (y[i] + y[i + 1]) * w
+
+    w0 = x[n - 1] * y[0] - x[0] * y[n - 1]
+    ax += (x[n - 1] + x[0]) * w0
+    ay += (y[n - 1] + y[0]) * w0
+
+    return [ax / 6.0 / area, ay / 6.0 / area]
