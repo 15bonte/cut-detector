8adc23d1fdbbd30ad7e40fdd604102111cedfdc5
15bonte <thomas.bonte95@gmail.com>
2024-07-22 12:34:41 +0200

    Remove useless image transformation for MT cut detection

--- src/cut_detector/utils/mt_cut_detection/bridges_mt_model_manager.py
+++ src/cut_detector/utils/mt_cut_detection/bridges_mt_model_manager.py
@@ -17,69 +17,6 @@ from .micro_tubules_augmentation import (
 )
 
 
-def transform_image_orientation(
-    image, margin=10, percentile=75
-) -> tuple[np.ndarray]:
-    """Compute image orientation and rotate it to have the bridges horizontal.
-
-    Parameters
-    ----------
-    image : np.ndarray
-        Image to rotate.
-    margin : int, optional
-        Margin to consider for orientation computation, by default 10.
-    percentile : int, optional
-        Percentile to consider for orientation computation, by default 75.
-
-    Returns
-    -------
-    tuple[np.ndarray]
-        Rotated image and binary image.
-    """
-    keep_3_channels = False
-    if len(image.shape) == 3:
-        assert image.shape[0] == 1, "Only one channel image is supported"
-        image = image[0]
-        keep_3_channels = True
-
-    original_height, original_width = image.shape
-
-    # Select sub-image (containing information)
-    sub_image = image[
-        original_height // 2 - margin : original_height // 2 + margin,
-        original_width // 2 - margin : original_width // 2 + margin,
-    ]
-    threshold = np.percentile(sub_image, percentile)
-    binary_image = (sub_image > threshold).astype(np.uint8)
-
-    # Compute image moments
-    moments = cv2.moments(binary_image)
-    # Compute orientation
-    orientation = 0.5 * np.arctan2(
-        2 * moments["mu11"], moments["mu20"] - moments["mu02"]
-    )
-    # Compute orientation in degrees
-    orientation_degrees = np.degrees(orientation)
-
-    # Rotate image and keep original size
-    rotated_image = ndimage.rotate(
-        image, orientation_degrees, cval=image.min()
-    )
-    height, width = rotated_image.shape
-    rotated_image = rotated_image[
-        (height - original_height) // 2 : (height - original_height) // 2
-        + original_height,
-        (width - original_width) // 2 : (width - original_width) // 2
-        + original_width,
-    ]
-
-    # Expand first dim
-    if keep_3_channels:
-        rotated_image = np.expand_dims(rotated_image, axis=0)
-
-    return rotated_image, binary_image
-
-
 class BridgesMtModelManager(CnnModelManager):
     """Model manager for Binary Bridges CNN classification models."""
 
@@ -99,13 +36,10 @@ class BridgesMtModelManager(CnnModelManager):
         input_arrays = input_tensor.detach().cpu().numpy()  # BCYX
 
         augmentation_tool = MicroTubulesAugmentation()
-        augmentation_tool.augmentations.pop("top")
-        augmentation_tool.augmentations.pop("bottom")
 
         augmented_array = []
-        for orig_input_array in input_arrays:
+        for input_array in input_arrays:
             # Put first dimension last
-            input_array, _ = transform_image_orientation(orig_input_array)
             augmented_images = augmentation_tool.generate_augmentations(
                 input_array
             )
