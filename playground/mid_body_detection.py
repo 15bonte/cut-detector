b933c5c2383045ab0e4814d7e1c8494385748de8
15bonte <thomas.bonte95@gmail.com>
2024-09-20 12:05:25 +0200

    Rewrite mid_body_detection playground to run whole process

--- playground/mid_body_detection.py
+++ playground/mid_body_detection.py
@@ -7,93 +7,53 @@ from typing import Optional
 from cnn_framework.utils.readers.tiff_reader import TiffReader
 
 from cut_detector.data.tools import get_data_path
-from cut_detector.factories.mid_body_detection_factory import (
-    MidBodyDetectionFactory,
-)
-from cut_detector.utils.mid_body_detection.tracking import TRACKING_FUNCTIONS
-from cut_detector.utils.mitosis_track import MitosisTrack
 from cut_detector.utils.tools import re_organize_channels
-from cut_detector.utils.track_generation import generate_tracks_from_spots
+from cut_detector.widget_functions.mid_body_detection import (
+    perform_mid_body_detection,
+)
 
 
 def main(
-    image_path: Optional[str] = os.path.join(
-        get_data_path("mitosis_movies"), "example_video_mitosis_0_0_to_5.tiff"
-    ),
-    mitosis_path: Optional[str] = get_data_path("mitoses"),
-    show_points: bool = True,
-    show_tracks: bool = True,
+    videos_path: Optional[str] = get_data_path("videos"),
+    video_name: Optional[str] = "example_video",
+    mitoses_path: Optional[str] = get_data_path("mitoses"),
+    tracks_path: Optional[str] = get_data_path("tracks"),
+    save: Optional[bool] = False,
 ) -> None:
     """
     Parameters
     ----------
-    image_path : str
-        Path to the image to process.
-    mitosis_path : str
+    videos_path : str
+        Path to the videos folder.
+    video_name : str
+        Name of the video.
+    mitoses_path : str
         Path to the mitoses data.
-    show_points : bool
-        If True, plot the detected spots.
-    show_tracks : bool
-        If True, plot the detected tracks.
+    tracks_path : str
+        Path to the tracks data.
+    save : bool
+        If True, update mitoses.
     """
 
-    # If paths are directories, take their first file
-    if os.path.isdir(image_path):
-        image_path = os.path.join(image_path, os.listdir(image_path)[0])
-    if os.path.isdir(mitosis_path):
-        mitosis_path = os.path.join(mitosis_path, os.listdir(mitosis_path)[0])
+    videos_path = os.path.join(videos_path, f"{video_name}.tif")
 
-    # Read data: image, mitosis_track
-    image = TiffReader(image_path, respect_initial_type=True).image  # TCZYX
-    with open(mitosis_path, "rb") as f:
-        mitosis_track = MitosisTrack.load(f)
+    # Read image and preprocess if needed
+    image = TiffReader(videos_path, respect_initial_type=True).image  # TCZYX
+    image = image.squeeze()  # TCYX
+    image = re_organize_channels(image)  # TYXC
 
-    mitosis_movie = image[:, :3, ...].squeeze()  # T C=3 YX
-    mitosis_movie = re_organize_channels(mitosis_movie)  # TYXC
-
-    # Search for mid-body in mitosis movie
-    factory = MidBodyDetectionFactory()
-
-    spots_candidates = factory.detect_mid_body_spots(
-        mitosis_movie=mitosis_movie,
-        method="difference_gaussian",
-        parallelization=False,
-        mitosis_track=mitosis_track,
+    a = perform_mid_body_detection(
+        image, video_name, mitoses_path, tracks_path, save=save
     )
-
-    if show_points:
-        print("\nSpots candidates:")
-        for frame, spots in spots_candidates.items():
-            for spot in spots:
-                print(
-                    {
-                        "fr": frame,
-                        "x": spot.x,
-                        "y": spot.y,
-                        "mklp_int": spot.intensity,
-                        "sir_int": spot.sir_intensity,
-                    }
-                )
-
-    generate_tracks_from_spots(
-        spots_candidates, TRACKING_FUNCTIONS["spatial_laptrack"]
-    )
-
-    if show_tracks:
-        print("\nSpots candidates with tracks:")
-        for frame, spots in spots_candidates.items():
-            for spot in spots:
-                print(
-                    {
-                        "fr": frame,
-                        "x": spot.x,
-                        "y": spot.y,
-                        "mklp_int": spot.intensity,
-                        "sir_int": spot.sir_intensity,
-                        "track_id": spot.track_id,
-                    }
-                )
+    b = 0
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        videos_path=r"C:\Users\thoma\data\Data Pasteur\main_data\videos",
+        video_name="t1_NG-MKLP1-clone-1-1",
+        mitoses_path=r"C:\Users\thoma\data\Data Pasteur\main_data\mitoses_new",
+        tracks_path=r"C:\Users\thoma\data\Data Pasteur\main_data\exported_tracks_new",
+        save=False,
+    )
+    # main()
