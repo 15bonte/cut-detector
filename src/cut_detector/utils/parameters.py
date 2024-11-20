44acc653ecad3eb52e10aeadf805d397e5ecba1e
15bonte <thomas.bonte95@gmail.com>
2024-11-20 11:51:23 +0100

    Create parameters class

---
+++ src/cut_detector/utils/parameters.py
@@ -0,0 +1,36 @@
+"""Video parameters for the cut detector."""
+
+
+class Parameters:
+    """
+    Class to store video parameters for the cut detector.
+
+    Attributes
+    ----------
+    spatial_resolution : int
+        Spatial resolution of the video.
+        In nanometers per pixel, for example 100 means 1 pixel is 100 nm.
+    time_resolution : int
+        Time resolution of the video.
+        In minutes per frame, for example 10 means 1 frame every 10 minutes.
+    """
+
+    def __init__(self, spatial_resolution=225, time_resolution=10):
+        self.spatial_resolution = spatial_resolution
+        self.time_resolution = time_resolution
+
+        # Range to look for metaphase candidate spots
+        metaphase_interval = 100  # minutes
+        self.frames_around_metaphase = metaphase_interval / time_resolution
+
+        # Range to look for midbody in between cells
+        cytokinesis_interval = 200  # minutes
+        self.cytokinesis_duration = cytokinesis_interval / time_resolution
+
+        # Channels
+        self.mid_body_channel = 1
+        self.sir_channel = 0
+
+        # Classes indexes
+        self.interphase_index = 0
+        self.metaphase_index = 1
